e9fd464667b611254e8f627977a1bc639ba07cf1
viktor <radchenkovictor@yahoo.com>
2020-08-04 23:56:18 +0400

    react + django work together

--- gatsby/settings.py
+++ gatsby/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '76en6$=noyek362&nw069%4gc*f8ml9&4)p2^y6t66&q5bgz10'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '192.168.0.153', '127.0.0.1']
 
 
 # Application definition
@@ -60,7 +60,9 @@ ROOT_URLCONF = 'gatsby.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/build'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,6 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/build/static')
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
