89d51cb5606b0a2a3d2affb5ddba5d756864c6a2
viktor <radchenkovictor@yahoo.com>
2020-08-10 09:22:39 +0400

    pre-deploy

---
+++ frontend/build/precache-manifest.c04dbda31802f289f2533dea0e7727c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656ebdd5b1725f8c6a532fd6fd8aa91e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62ac66bd87b03840281f",
+    "url": "/static/css/main.75c467d5.chunk.css"
+  },
+  {
+    "revision": "8802578f4fa4a92a0423",
+    "url": "/static/js/2.aba56b15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aba56b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62ac66bd87b03840281f",
+    "url": "/static/js/main.9edf61fa.chunk.js"
+  },
+  {
+    "revision": "d472ebac28f362dd2f88",
+    "url": "/static/js/runtime-main.08fcf510.js"
+  }
+]);
